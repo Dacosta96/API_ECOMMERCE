67090c508a10e9a3740a814e59e88a730cfc312e
DACOSTA96 <diego.ac9614@gmail.com>
2024-04-15 16:56:15 -0500

    impletation fake data

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 const express = require('express');
+const {faker} = require('@faker-js/faker')
+
 const app = express();
-const port = 3002;
+const port = 3000;
 
 app.get('/', (req,res)=>{
   res.send('hola servidor')
@@ -10,9 +12,22 @@ app.listen(port,()=>{
   console.log('estoy utilizando el puerto'+port);
 })
 
+
 app.get('/products', (req,res)=>{
-  res.json({
-    name:'name',
-    price: 2500
-  })
-});
+
+  const products =[];
+  const {size} = req.query;
+  const limit = size || 20;
+
+  for (index =0; index < limit ; index++){
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(),10),
+      image: faker.image.url(),
+
+    })
+  }
+
+  res.json(products);
+
+})
