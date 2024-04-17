fd01ee177b54cc99bb572c337335434d33d6b490
DACOSTA96 <diego.ac9614@gmail.com>
2024-04-17 14:19:19 -0500

    creation routes

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
+const express=require('express');
+const routerApi=require('./Routes');
+const app=express();
+const port=3000;
 
-const express = require('express');
-const {faker} = require('@faker-js/faker')
+app.get('/',(req,res)=>{
+  res.send('Hola mi server en express');
+  });
 
-const app = express();
-const port = 3000;
-
-app.get('/', (req,res)=>{
-  res.send('hola servidor')
-});
-app.listen(port,()=>{
-  console.log('estoy utilizando el puerto'+port);
-})
+  app.get('/nueva-ruta',(req,res)=>{
+    res.send('Hola, soy una nueva ruta');
+  });
 
 
-app.get('/products', (req,res)=>{
-
-  const products =[];
-  const {size} = req.query;
-  const limit = size || 20;
-
-  for (index =0; index < limit ; index++){
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(),10),
-      image: faker.image.url(),
-
-    })
-  }
-
-  res.json(products);
-
-})
+  routerApi(app);
+  app.listen(port,()=>{
+    console.log('Mi port'+port);
+  });
